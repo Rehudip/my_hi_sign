44a34536bbee6077f08d5cd23eadcf8e9fe470a1
Rehudip <81365315+Rehudip@users.noreply.github.com>
2023-09-22 10:58:08 +0900

    4_cal.py ADD

---
+++ cal_4.py
@@ -0,0 +1,16 @@
+def plus(x,y):
+	return x+y
+def minus(x, y):
+	return x-y
+def multiply(x,y):
+	return x*y
+def devide(x,y):
+	return x/y
+
+x = 10
+y = 20
+
+print(plus(x,y))
+print(minus(x,y))
+print(multiply(x,y))
+print(device(x,y))
